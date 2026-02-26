ff6e8f81a2ad43ca61697a9542b4373229f8f63b
Luise Haeuser <haeusele@cascade-login.cluster>
2026-02-26 11:43:28 +0100

    minor updates

--- predict_difficulties.py
+++ predict_difficulties.py
@@ -1,41 +1,47 @@
 import os
 import pandas as pd
+import subprocess
 
 from pypythia.prediction import predict_difficulty
+from pypythia.raxmlng import RAxMLNG
 import pathlib
 
-def predict(msa_path):
+def predict(msa_path, rax):
+    print(msa_path)
     msa = pathlib.Path(msa_path)
-    return predict_difficulty(msa)
+    return predict_difficulty(msa, raxmlng = rax)
 
-data_dir = "data/treebase/msa"
-difficulties = []
-for ds in os.listdir(data_dir):
-    msa_path = os.path.join(data_dir, ds)
-    if not os.path.isfile(msa_path):
-        print("Skipping", ds)
-        continue
-    try:
-        difficulties.append([ds.split(".")[0], predict(msa_path)])
-    except Exception as e:
-        print(e)
-        continue
-df = pd.DataFrame(difficulties, columns=["dataset", "difficult_predict"])
-df.to_csv("data/treebase/difficulty_predictions.csv")
+rax = pathlib.Path("./snakemake/raxml-ng")
+#rax = RAxMLNG(exe_path)
+
+
+#data_dir = "data/treebase/msa"
+#difficulties = []
+#for ds in os.listdir(data_dir):
+#    msa_path = os.path.join(data_dir, ds)
+#    if not os.path.isfile(msa_path):
+#        print("Skipping", ds)
+#        continue
+#    try:
+#        difficulties.append([ds.split(".")[0], predict(msa_path)])
+#    except Exception as e:
+#        print(e)
+#        continue
+#df = pd.DataFrame(difficulties, columns=["dataset", "difficulty_prediction"])
+#df.to_csv("data/treebase/difficulty_prediction.csv")
 
 data_dir = "data/sim/msa"
-#data_dir = "data/sim_difficult/msa"
-lengths = []
+difficulties = []
 for ds in os.listdir(data_dir):
     msa_path = os.path.join(data_dir, ds, "gtr_g_sim_msa.fasta")
     if not os.path.isfile(msa_path):
         print("Skipping", ds)
         continue
     try:
-        difficulties.append([ds.split(".")[0], predict(msa_path)])
+        difficulties.append([ds.split(".")[0], predict(msa_path, rax)])
     except Exception as e:
         print(e)
         continue
-df = pd.DataFrame(difficulties, columns=["dataset", "difficult_predict"])
-df.to_csv("data/sim/difficulty_predictions.csv")
+df = pd.DataFrame(difficulties, columns=["dataset", "difficulty_prediction"])
+df.to_csv("data/sim/difficulty_prediction.csv")
 
--- sizes.py
+++ sizes.py
@@ -12,13 +12,12 @@ for ds in os.listdir(data_dir):
         continue
     with open(msa_path, "r") as msa_file:
         line = msa_file.readlines()[0]
-        length = int(line.split(" ")[2])
+        length = int(line.split(" ")[1])
         lengths.append([ds.split(".")[0], length])
 df = pd.DataFrame(lengths, columns=["dataset", "size"])
 df.to_csv("data/treebase/sizes.csv")
 
-#data_dir = "data/sim/msa"
-data_dir = "data/sim_difficult/msa"
+data_dir = "data/sim/msa"
 lengths = []
 for ds in os.listdir(data_dir):
     msa_path = os.path.join(data_dir, ds, "gtr_g_sim_msa.fasta")
@@ -30,5 +29,4 @@ for ds in os.listdir(data_dir):
         length = content.count(">")
         lengths.append([ds.split(".")[0], length])
 df = pd.DataFrame(lengths, columns=["dataset", "size"])
-#df.to_csv("data/sim/sizes.csv")
-df.to_csv("data/sim_difficult/sizes.csv")
+df.to_csv("data/sim/sizes.csv")
